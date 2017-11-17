8a31153f6aa18735225719deaefdf403d6f8fda0
nrakochy <nick.rakochy@gmail.com>
2017-11-17 00:34:41 -0600

    Extracted middleware from interceptor init

--- src/gdax_clj/http.clj
+++ src/gdax_clj/http.clj
@@ -1,67 +1,33 @@
 (ns gdax-clj.http
-  (:require [clojure.set :as set]
+  (:require [gdax-clj.middleware :as mid] 
             [ajax.core :as ajax]))
 
-
-(defn response-handler
-  "Hand ajax response back to caller"
-  [[ok response]] response)
-
-(defn set-handler [request]
-  (update request :handler response-handler))
-
-(defn sign-token [params])
-
-(defn set-auth-map
-  [{:keys [params]
-    :or   {params {}}
-    :as   request}]
-  (let [token (sign-token params)]
-                 {:accept        "application/json"
-                  :cb-access-sign " "
-                  :cb-access-timestamp " "
-                  :cb-access-passphrase " "
-                  :cb-access-key token}))
-
-(defn set-auth-headers
-  "Adds gdax-specific headers to the request.
-  This must be the last interceptor run for the message signing to work correctly"
-  [{:keys [headers]
-    :or   {headers {}}
-    :as   request}]
-      (->> (set-auth-map request)
-           (merge headers)
-           (assoc request :headers)))
-  (set-auth-headers {:a 1})
-
+(def build-uri
+  (ajax/to-interceptor {:name "Create uri from path"
+                        :request mid/set-uri}))
 (def add-handler
   (ajax/to-interceptor {:name "Return response handler"
-                        :request set-handler}))
+                        :request mid/set-handler}))
 (def auth-headers
   (ajax/to-interceptor {:name   "Decorate headers with auth"
-                        :request set-auth-headers}))
+                        :request mid/set-auth-headers}))
 (def default-opts
   {:timeout         5000
    :format          (ajax/json-request-format)
-   :interceptors    [ajax/default-interceptors add-handler auth-headers]
+   :interceptors    [ajax/default-interceptors add-handler build-uri auth-headers]
    :response-format (ajax/json-response-format {:keywords? true})})
 
-;;- to spec -- required keys: uri, handler
+;;- to spec -- required keys: path, handler
 ;;- required output keys from ajax-cljs api
-(defn set-ajax-params [{:as m
-                         :keys [interceptors]
-                         :or {interceptors []}}]
-  (let [interceptor-map {:interceptors (into [] (concat interceptors (:interceptors default-opts)))}]
-    (merge-with into interceptor-map m default-opts)))
+(defn set-ajax-params [m]
+    (merge m default-opts))
 
-(defn- gdax-ajax [m]
-  (-> m set-ajax-params ajax/ajax-request))
+(defn gdax-ajax [m]
+  (-> m set-ajax-params)) ;;ajax/ajax-request))
 
-;;- Additionally, post request will need params key even if empty
-(defn gdax-post [m]
-  (gdax-ajax (assoc m :method :post)))
+(def req {:method "GET"
+          :resource :account
+          :endpoint-type :rest
+          :account-id 1})
 
-;;- to spec -- required keys: uri
-;;- required output keys from ajax-cljs api
-(defn gdax-get [m]
-  (gdax-ajax (assoc m :method :get)))
+(set-ajax-params req)
---
+++ src/gdax_clj/middleware.cljc
@@ -0,0 +1,51 @@
+(ns gdax-clj.middleware
+  (:require         [clojure.spec.alpha :as s]
+                    [gdax-clj.auth :as auth]
+                    [selmer.parser :as parser]))
+
+(def url-resource {:rest "https://api.gdax.com"
+                   :fix  "https://fix.gdax.com:4198"
+                   :ws    "wss://ws-feed.gdax.com"})
+
+(def routes {"GET" {:account {:route "/accounts/{{account-id}}"
+                              :required-keys [:account-id]}}
+             "POST" {}
+             "DELETE" {}})
+
+(defn response-handler
+  "Hand ajax response back to caller"
+  [[ok response]] response)
+
+(defn set-handler [request]
+  (assoc request :handler response-handler))
+
+(defn set-auth-headers
+  "Adds gdax-specific headers to the request.
+  This must be the last interceptor run for the message signing to work correctly"
+  [{:keys [headers]
+    :or   {headers {}}
+    :as   request}]
+  (->> (auth/get-auth-map request)
+       (merge headers)
+       (assoc request :headers)))
+
+(defn set-uri [{:keys [method resource endpoint-type] :as request}]
+  (let [{:keys [route]} (get-in routes [method resource])
+        relative-path   (parser/render route request)
+        domain          (get url-resource endpoint-type)]
+    (-> request
+        (assoc :path relative-path)
+        (assoc :uri (str domain relative-path)))))
+
+(def default-required-keys [::api-key ::secret ::password])
+(s/fdef ::configure-request
+        :args (s/cat :request map?)
+        :ret map?)
+         ;; :fn (fn [ctx]
+         ;;         (= (into #{} (-> ctx :args :ks)))))
+         ;;      (into #{} (-> ctx :ret keys))))
+
+(def req {:method "GET"
+          :resource :account
+          :endpoint-type :rest
+          :account-id 1})
