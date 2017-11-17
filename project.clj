0887e6c07f14240671ca1d014adfedf2d4e683bf
nrakochy <nick.rakochy@gmail.com>
2017-11-17 00:37:05 -0600

    Updated deps

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+                 [selmer "1.11.3"]
                  [cljs-ajax "0.7.3"]])
 
